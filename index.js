2729269a53262aad96457eab53296e7127dc4bb3
Wakako <wakako.nishimura@gmail.com>
2023-08-23 17:37:55 -0500

    added and removed jquery functions

--- index.js
+++ index.js
@@ -2,3 +2,38 @@ function getPassword() {
     const password = document.getElementById('exampleInputPassword1').value
     console.log(password)
 }
+
+// jQuery(function($) {
+//     $(window).on('scroll', function() {
+//       console.log($(this).scrollTop());
+// 		if ($(this).scrollTop() >= 100) {
+// 			$('.navbar').addClass('fixed-top');
+// 		} else if ($(this).scrollTop() == 0) {
+// 			$('.navbar').removeClass('fixed-top');
+// 		}
+// 	});
+	
+// 	function adjustNav() {
+// 		var winWidth = $(window).width(),
+// 			dropdown = $('.dropdown'),
+// 			dropdownMenu = $('.dropdown-menu');
+		
+// 		if (winWidth >= 768) {
+// 			dropdown.on('mouseenter', function() {
+// 				$(this).addClass('show')
+// 					.children(dropdownMenu).addClass('show');
+// 			});
+			
+// 			dropdown.on('mouseleave', function() {
+// 				$(this).removeClass('show')
+// 					.children(dropdownMenu).removeClass('show');
+// 			});
+// 		} else {
+// 			dropdown.off('mouseenter mouseleave');
+// 		}
+// 	}
+	
+// 	$(window).on('resize', adjustNav);
+	
+// 	adjustNav();
+// });
